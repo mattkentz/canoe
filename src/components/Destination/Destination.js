e0d6c8ff2cfd0b34daceb600c69a2f57811f116e
Matthias Kentzia <matthias.kentzia@tipico.com>
2016-12-17 15:57:18 +0100

    #Refactored Destination component and container

--- src/components/Destination/Destination.js
+++ src/components/Destination/Destination.js
@@ -1,33 +1,16 @@
-import React, { Component }  from 'react';
+import React  from 'react';
 import './Destination.scss';
 import Trips from '../../containers/Trips/Trips';
 
-
-class Destination extends Component {
-
-  componentWillMount () {
-    const destinationId = this.props.routeParams.id || this.props.id;
-    if (destinationId) {
-      this.destination = this.props.destinations[destinationId]
-    }
-  }
-
-  addDestinationToTrip(index) {
-    this.props.addToTrip(index, this.destination);
-  }
-
-  render () {
-    const destination = this.destination;
-
-    return (
-      <div className="destination">
-        <h1>{destination.name}</h1>
-        <hr/>
-        <h4>Add To Trip:</h4>
-        <Trips handleClick={this.addDestinationToTrip.bind(this)}></Trips>
-      </div>
-    )
-  }
+const Destination = ({destination, handleTripsClick}) => {
+  return (
+    <div className="destination">
+      <h1>{destination.name}</h1>
+      <hr/>
+      <h4>Add To Trip:</h4>
+      <Trips handleClick={handleTripsClick}></Trips>
+    </div>
+  )
 }
 
 export default Destination;
--- src/containers/Destination/Destination.js
+++ src/containers/Destination/Destination.js
@@ -1,6 +1,27 @@
+import React, { Component }  from 'react';
 import { connect } from 'react-redux';
 import Destination from '../../components/Destination/Destination';
 import * as actions from './Destination.actions';
+
+class DestinationComponent extends Component {
+
+  componentWillMount () {
+    const destinationId = this.props.routeParams.id || this.props.id;
+    if (destinationId) {
+      this.destination = this.props.destinations[destinationId]
+    }
+  }
+
+  addDestinationToTrip(index) {
+    this.props.addToTrip(index, this.destination);
+  }
+
+  render () {
+    return (
+      <Destination destination={this.destination} handleTripsClick={this.addDestinationToTrip.bind(this)}></Destination>
+    )
+  }
+}
 
 const mapStateToProps = (state, props) => {
   return {
@@ -14,6 +35,6 @@ const mapDispatchToProps = (dispatch) => {
   }
 };
 
-const DestinationContainer = connect(mapStateToProps, mapDispatchToProps)(Destination);
+const DestinationContainer = connect(mapStateToProps, mapDispatchToProps)(DestinationComponent);
 
 export default DestinationContainer;
