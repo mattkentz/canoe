d165b7d177b7d1d562a603e1c97d569f03bd3407
Matthias Kentzia <matthias.kentzia@tipico.com>
2016-12-17 16:04:09 +0100

    #Refactored Trip component and container

--- src/components/Trip/Trip.js
+++ src/components/Trip/Trip.js
@@ -1,29 +1,16 @@
-import React, { Component }  from 'react';
+import React  from 'react';
 import './Trip.scss';
 import Destinations from '../../components/Destinations/Destinations';
 
-
-class Trip extends Component {
-
-    componentWillMount () {
-        const tripId = this.props.routeParams.id;
-        if (tripId) {
-            this.trip = this.props.trips[tripId]
-        }
-    }
-
-    render () {
-        const trip = this.trip;
-
-        return (
-            <section>
-                <h1>{trip.name}</h1>
-                <hr/>
-                <h4>Destinations</h4>
-                <Destinations destinations={trip.destinations}></Destinations>
-            </section>
-        )
-    }
-}
+const Trip = ({trip}) => {
+  return (
+    <section>
+      <h1>{trip.name}</h1>
+      <hr/>
+      <h4>Destinations</h4>
+      <Destinations destinations={trip.destinations}></Destinations>
+    </section>
+  )
+};
 
 export default Trip;
--- src/containers/Trip/Trip.js
+++ src/containers/Trip/Trip.js
@@ -1,5 +1,22 @@
+import React, { Component }  from 'react';
 import { connect } from 'react-redux';
 import Trip from '../../components/Trip/Trip';
+
+class TripComponent extends Component {
+
+    componentWillMount () {
+        const tripId = this.props.routeParams.id || this.props.id;
+        if (tripId) {
+            this.trip = this.props.trips[tripId]
+        }
+    }
+
+    render () {
+        return (
+          <Trip trip={this.trip}></Trip>
+        )
+    }
+}
 
 const mapStateToProps = (state, props) => {
     return {
@@ -11,6 +28,6 @@ const mapDispatchToProps = (dispatch) => {
     return {}
 };
 
-const TripContainer = connect(mapStateToProps, mapDispatchToProps)(Trip);
+const TripContainer = connect(mapStateToProps, mapDispatchToProps)(TripComponent);
 
 export default TripContainer;
