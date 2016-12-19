e0aa839d01a4d183c98b2205a2be50ea6cc2f8d5
Matthias Kentzia <matthias.kentzia@tipico.com>
2016-12-19 20:42:36 +0100

    #Changed unit test structure for DestinationForm

--- src/containers/DestinationForm/DestinationForm.test.js
+++ src/containers/DestinationForm/DestinationForm.test.js
@@ -6,122 +6,124 @@ import * as reducers from './DestinationForm.reducers';
 import * as constants from './DestinationForm.constants';
 import * as sagas from './DestinationForm.sagas';
 
-describe('Destination Form Actions - ', function () {
-    describe('formFieldUpdate()', function () {
-        it('should return an object with an action with type, field name and field value', function () {
-            const action = actions.formFieldUpdate('search', 'Germany');
-            expect(action).to.have.property('type', constants.UPDATE_DESTINATION_FORM);
-            expect(action).to.have.property('field', 'search');
-            expect(action).to.have.property('value', 'Germany');
+describe('Destination Form ', function () {
+    describe('Actions - ', function () {
+        describe('formFieldUpdate()', function () {
+            it('should return an object with an action with type, field name and field value', function () {
+                const action = actions.formFieldUpdate('search', 'Germany');
+                expect(action).to.have.property('type', constants.UPDATE_DESTINATION_FORM);
+                expect(action).to.have.property('field', 'search');
+                expect(action).to.have.property('value', 'Germany');
+            });
+        });
+        describe('fetchDestinations()', function () {
+            it('should return an object with an action with type, field name and field value', function () {
+                const action = actions.fetchDestinations('Germany');
+                expect(action).to.have.property('type', constants.FETCH_DESTINATION_REQUESTED);
+                expect(action).to.have.property('value', 'Germany');
+            });
+        });
+        describe('fetchDestinationsSuccess()', function () {
+            it('should return an object with an action with type, field name and field value', function () {
+                const destinations = [
+                    { name: 'Canada' },
+                    { name: 'Portugal' }
+                ];
+                const action = actions.fetchDestinationsSuccess(destinations);
+                expect(action).to.have.property('type', constants.FETCH_DESTINATION_SUCCESS);
+                expect(action).to.have.property('destinations', destinations);
+            });
+        });
+        describe('fetchDestinationsFailed()', function () {
+            it('should return an object with an action with type, field name and field value', function () {
+                const action = actions.fetchDestinationsFailed('Error');
+                expect(action).to.have.property('type', constants.FETCH_DESTINATION_FAILED);
+                expect(action).to.have.property('message', 'Error');
+            });
         });
     });
-    describe('fetchDestinations()', function () {
-        it('should return an object with an action with type, field name and field value', function () {
-            const action = actions.fetchDestinations('Germany');
-            expect(action).to.have.property('type', constants.FETCH_DESTINATION_REQUESTED);
-            expect(action).to.have.property('value', 'Germany');
-        });
-    });
-    describe('fetchDestinationsSuccess()', function () {
-        it('should return an object with an action with type, field name and field value', function () {
-            const destinations = [
-                { name: 'Canada' },
-                { name: 'Portugal' }
-            ];
-            const action = actions.fetchDestinationsSuccess(destinations);
-            expect(action).to.have.property('type', constants.FETCH_DESTINATION_SUCCESS);
-            expect(action).to.have.property('destinations', destinations);
-        });
-    });
-    describe('fetchDestinationsFailed()', function () {
-        it('should return an object with an action with type, field name and field value', function () {
-            const action = actions.fetchDestinationsFailed('Error');
-            expect(action).to.have.property('type', constants.FETCH_DESTINATION_FAILED);
-            expect(action).to.have.property('message', 'Error');
-        });
-    });
-});
 
-describe('Destination Form Reducers - ', function () {
-    describe('destinationForm()', function () {
-        it('should add a new field to the state', function () {
-            const action = {
-                type: constants.UPDATE_DESTINATION_FORM,
-                field: 'search',
-                value: 'Italy'
-            }
-            const newState = reducers.destinationForm(null, action);
-
-            expect(newState).to.have.property('search', 'Italy');
-        });
-
-        it('should add a new field to the state', function () {
-            const initialState = {
-                'search': 'Italy'
-            }
-
-            const action = {
-                type: constants.UPDATE_DESTINATION_FORM,
-                field: 'search',
-                value: 'England'
-            }
-
-            const newState = reducers.destinationForm(initialState, action);
-
-            expect(newState).to.have.property('search', 'England');
-        });
-    });
-});
-
-describe('Destination Form Sagas - ', function () {
-    describe('fetchDestinations()', function () {
-        it('should return a FETCH_DESTINATION_SUCCESS action', () => {
-            const action = {
-                value: 'France'
-            };
-
-            const destinations = [
-                {
-                    name: 'France',
-                    ISO: 'FR'
+    describe('Reducers - ', function () {
+        describe('destinationForm()', function () {
+            it('should add a new field to the state', function () {
+                const action = {
+                    type: constants.UPDATE_DESTINATION_FORM,
+                    field: 'search',
+                    value: 'Italy'
                 }
-            ];
+                const newState = reducers.destinationForm(null, action);
 
-            const generator = sagas.fetchDestinations(action);
+                expect(newState).to.have.property('search', 'Italy');
+            });
 
-            let next = generator.next(actions.fetchDestinations(action.value));
+            it('should add a new field to the state', function () {
+                const initialState = {
+                    'search': 'Italy'
+                }
 
-            expect(next.value).to.deep.equal(call(api.fetchDestinations, action.value));
+                const action = {
+                    type: constants.UPDATE_DESTINATION_FORM,
+                    field: 'search',
+                    value: 'England'
+                }
 
-            next = generator.next(destinations);
+                const newState = reducers.destinationForm(initialState, action);
 
-            expect(next.value).to.deep.equal(
-                put({
-                    type: constants.FETCH_DESTINATION_SUCCESS,
-                    destinations: destinations
-                })
-            );
+                expect(newState).to.have.property('search', 'England');
+            });
         });
+    });
 
-        it('should return a FETCH_DESTINATION_FAILED action', () => {
-            const action = {
-                value: 'ABC'
-            };
+    describe('Sagas - ', function () {
+        describe('fetchDestinations()', function () {
+            it('should return a FETCH_DESTINATION_SUCCESS action', () => {
+                const action = {
+                    value: 'France'
+                };
 
-            const generator = sagas.fetchDestinations(action);
+                const destinations = [
+                    {
+                        name: 'France',
+                        ISO: 'FR'
+                    }
+                ];
 
-            let next = generator.next(actions.fetchDestinations(action.value));
+                const generator = sagas.fetchDestinations(action);
 
-            expect(next.value).to.deep.equal(call(api.fetchDestinations, action.value));
+                let next = generator.next(actions.fetchDestinations(action.value));
 
-            next = generator.throw("Error");
+                expect(next.value).to.deep.equal(call(api.fetchDestinations, action.value));
 
-            expect(next.value).to.deep.equal(
-                put({
-                    type: constants.FETCH_DESTINATION_FAILED,
-                    message: "Error"
-                })
-            );
+                next = generator.next(destinations);
+
+                expect(next.value).to.deep.equal(
+                  put({
+                      type: constants.FETCH_DESTINATION_SUCCESS,
+                      destinations: destinations
+                  })
+                );
+            });
+
+            it('should return a FETCH_DESTINATION_FAILED action', () => {
+                const action = {
+                    value: 'ABC'
+                };
+
+                const generator = sagas.fetchDestinations(action);
+
+                let next = generator.next(actions.fetchDestinations(action.value));
+
+                expect(next.value).to.deep.equal(call(api.fetchDestinations, action.value));
+
+                next = generator.throw("Error");
+
+                expect(next.value).to.deep.equal(
+                  put({
+                      type: constants.FETCH_DESTINATION_FAILED,
+                      message: "Error"
+                  })
+                );
+            });
         });
     });
 });
